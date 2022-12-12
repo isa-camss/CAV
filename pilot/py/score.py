d908c33c2251c02ef12e7c94ed12d608df7dafa3
hricolor <60606496+hricolor@users.noreply.github.com>
2022-12-12 16:54:18 +0100

    CAV 1.1.0 updates

--- pilot/py/score.py
+++
@@ -1,8 +0,0 @@
-import sparql as s
-import electre as e
-
-
-def run():
-    r = s.run()           # Returns the results of the SPARQL query as a JSON
-    return e.run(r)       # Applies the ELECTRE-II algorithm and produces the scores for each criterion
-
